2415dae7e9a5f94fd7c605e9c60992e5d8a8882d
scratchsay <unconfigured@null.spigotmc.org>
2023-12-31 16:39:28 -0800

    updating apostrophe type consistency

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-var quotes = ["Programs must be written for people to read, and only incidentally for machines to execute.", "I’m not a great programmer; I’m just a good programmer with great habits.", "First, solve the problem. Then, write the code.", "The computer was born to solve problems that did not exist before.", "Good code is its own best documentation."];
+var quotes = ["Programs must be written for people to read, and only incidentally for machines to execute.", "I'm not a great programmer; I'm just a good programmer with great habits.", "First, solve the problem. Then, write the code.", "The computer was born to solve problems that did not exist before.", "Good code is its own best documentation."];
 var authors = ["Harold Abelson", "Kent Beck", "John Johnson", "Bill Gates", "Steve McConnell"];
 var randomNumber = Math.random();
 var randomIndex = Math.floor(randomNumber * quotes.length);
